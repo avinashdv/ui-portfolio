2dbd81e0b1373856f2faad12d06164d007949a86
avinashdv <avinash.dv.264@gmail.com>
2025-01-08 01:09:28 +0530

    resolved build issues with version

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
